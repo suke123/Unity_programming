8abcb9a54a958475adf71c7d56ff51b24cce8bc5
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-24 11:02:48 +0900

    追加

---
+++ Cannon01/Assets/BulletBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    GameObject ExplosionPrefab;
+    void OnCollisionEnter(Collision WHO){
+        Instantiate(
+            ExplosionPrefab,
+            transform.position,
+            transform.rotation
+            );
+
+        Destroy(gameObject);
+    }
+}
