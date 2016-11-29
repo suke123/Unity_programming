2bcb3a6985b720f5f5d40e39e47d5ee42435e9d7
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-29 19:05:33 +0900

    編集

---
+++ Cannon01/Assets/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+    public GameObject enemy;   //敵オブジェクト
+    public Transform ground;   //地面オブジェクト
+    public float count = 5;    //一度に何体のオブジェクトをスポーンさせるか
+    public float interval = 5; //何秒おきに敵を発生させるか
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();  //初期のスポーン
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;  //経過時間の加算
+        if (timer >= interval)
+        {
+            Spawn();
+            timer = 0;
+        }
+	}
+
+    void Spawn()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            float x = Random.Range();
+            float z = Random.Range();
+            Vector3 pos = new Vector3(x, 5, z) + ground.position;
+            GameObject.Instantiate(enemy, pos, Quaternion.identity);
+        }
+    }
+}
