d81f7dd67dc0e779c3f7c6726a3054bd5e3a12c8
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-27 09:23:06 +0900

    編集
    大砲の縦・横の動きを制限したい

--- Cannon01/Assets/script/Gun.cs
+++ Cannon01/Assets/script/Gun.cs
@@ -5,10 +5,12 @@ public class Gun : MonoBehaviour {
 
     public GameObject BulletPrefab;
     public GameObject SparkPrefab;
-    public float gun_speed = 50;
+    public float gun_speed = 50;                            //弾のスピード
+    public float cannon_x = Input.GetAxis("Vertical");      //大砲の立野動きの制限
+    public float cannon_y = Input.GetAxis("Horizontal");    //大砲の横の動きの制限
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 	}
 	
@@ -18,16 +20,22 @@ public class Gun : MonoBehaviour {
             Fire();
         }
 
-        transform.Rotate(
+        if(cannon_x >= 34 && cannon_x <= 92)
+        {
+            transform.Rotate(
             new Vector3(Input.GetAxis("Vertical") * 60.0f * Time.deltaTime, 0, 0),
             Space.Self
             );
+        }
 
-        Transform Base = transform.parent;
-        Base.Rotate(
-            new Vector3(0, Input.GetAxis("Horizontal") * 60.0f * Time.deltaTime, 0),
-            Space.World
-            );
+        if (cannon_y >= -43 && cannon_y <= 43)
+        {
+            Transform Base = transform.parent;
+            Base.Rotate(
+                new Vector3(0, Input.GetAxis("Horizontal") * 60.0f * Time.deltaTime, 0),
+                Space.World
+                );
+        }
 
     }
 
