1a07d8ccd822304b88d5bdefe5171f4d5562a3d0
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-12-27 20:00:45 +0900

    編集

--- Cannon01/Assets/script/Gun.cs
+++ Cannon01/Assets/script/Gun.cs
@@ -5,9 +5,10 @@ public class Gun : MonoBehaviour {
 
     public GameObject BulletPrefab;
     public GameObject SparkPrefab;
-    public float gun_speed = 50;                            //弾のスピード
-    public float cannon_x = Input.GetAxis("Vertical");      //大砲の立野動きの制限
-    public float cannon_y = Input.GetAxis("Horizontal");    //大砲の横の動きの制限
+    //public GameObject Base;
+    public float gun_speed = 50;
+    //public float cannon_x = Input.GetAxis("Vertical");
+    //public float cannon_y = Input.GetAxis("Horizontal");
 
     // Use this for initialization
     void Start () {
@@ -16,25 +17,20 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Fire();
-        }
+        Transform Base = transform.parent;
+        Base.Rotate(
+            new Vector3(0, Input.GetAxis("Horizontal") * 60.0f * Time.deltaTime, 0),
+            Space.World
+        );
 
-        if(cannon_x >= 34 && cannon_x <= 92)
-        {
-            transform.Rotate(
+        transform.Rotate(
             new Vector3(Input.GetAxis("Vertical") * 60.0f * Time.deltaTime, 0, 0),
             Space.Self
-            );
-        }
+        );
 
-        if (cannon_y >= -43 && cannon_y <= 43)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Transform Base = transform.parent;
-            Base.Rotate(
-                new Vector3(0, Input.GetAxis("Horizontal") * 60.0f * Time.deltaTime, 0),
-                Space.World
-                );
+            this.Fire();
         }
 
     }
